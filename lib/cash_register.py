67861b77d8fe7a4ee3350d91a6779d0f3ab0aaf7
Andy Patari <andy.patari@gmail.com>
2023-09-15 08:16:35 -0400

    passing tests

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0):
+    self.discount = discount
+    self.total = 0.0
+    self.items = []
+    
+
+  def add_item(self, str, price, quantity = 1):
+    for i in range(quantity):
+      self.items.append(str)
+    self.total += price * quantity
+    self.last = {
+      "name": str,
+      "price": price,
+      "quantity": quantity,
+    }
+
+  def void_last_transaction(self):
+    if self.last:
+      for i in range(self.last["quantity"]):
+        self.items.remove(self.last["name"])
+      self.total -= self.last["price"] * self.last["quantity"]
+      self.last = False
+
+  def apply_discount(self):
+    if self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      self.total -= self.discount * self.total / 100
+      report_val = self.total
+      if report_val % 1 == 0:
+        report_val = int(report_val)
+      print(f"After the discount, the total comes to ${report_val}.")
+  
+
